2d93ba8cc0b8f5dee8c95034d313b9ac6d7e94ea
hockeyiscool19 <jordaneisenman@gmail.com>
2023-10-05 17:44:13 -0400

    main

--- run.py
+++ run.py
@@ -1,20 +1,29 @@
-# from app import create_app
-
-# app = create_app()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 import argparse
 from app import create_app
 
 app = create_app()
 
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description="Run the Flask app")
+#     parser.add_argument("--host", type=str, default="127.0.0.1",
+#                         help="Hostname to run the app on. Default is 127.0.0.1.")
+#     parser.add_argument("--host", type=str, default="127.0.0.1",
+#                         help="Hostname to run the app on. Default is 127.0.0.1.")
+#     parser.add_argument("--port", type=int, default=5000,
+#                         help="Port to run the app on. Default is 5000.")
+
+#     args = parser.parse_args()
+
+#     app.run(debug=True, host=args.host, port=args.port)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the Flask app")
+    
+    # Only one --host argument is needed
     parser.add_argument("--host", type=str, default="127.0.0.1",
                         help="Hostname to run the app on. Default is 127.0.0.1.")
+    
     parser.add_argument("--port", type=int, default=5000,
                         help="Port to run the app on. Default is 5000.")
 
