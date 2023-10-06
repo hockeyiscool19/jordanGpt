a9630ed22666b9ea2375f69161a72f5e67366dbd
hockeyiscool19 <jordaneisenman@gmail.com>
2023-10-05 22:41:11 -0400

    stuff

--- PUBLIC_VARIABLES.py
+++ PUBLIC_VARIABLES.py
@@ -1,5 +1,6 @@
 import os
 import json
+
 # # Print all environmental variables
 # dev_env_file = 'dev.env'
 
@@ -19,3 +20,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 DATABASE_URL = os.environ.get('DATABASE_URL')
 FINE_TUNING_JOB = os.environ.get('FINE_TUNING_JOB')
 GCLOUD_AUTH = json.loads(os.environ.get('GCLOUD_AUTH'))
+
+gcloud_auth_str = os.environ.get('GCLOUD_AUTH')
+if not gcloud_auth_str:
+    raise ValueError("GCLOUD_AUTH environment variable is not set or is empty.")
+GCLOUD_AUTH = json.loads(gcloud_auth_str)
